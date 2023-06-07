3b162b4e0e633c9de599ace5a94a6c42b1294ac5
Govind Jangra <govind70821@gmail.com>
2023-06-07 16:27:39 +0530

    first commit

---
+++ Arrays/Pascaltriangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans(numRows);
+        ans[0].push_back(1);
+        for(int i=1;i<numRows;i++){
+            for(int j=0;j<i+1;j++){
+                if(j==0) ans[i].push_back(1);
+                else if(j==i) ans[i].push_back(1);
+                else ans[i].push_back(ans[i-1][j-1]+ans[i-1][j]);
+            }
+        }
+        return ans;
+    }
+};
